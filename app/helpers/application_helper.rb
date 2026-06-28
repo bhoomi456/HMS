504f98996a2653e06174087d7ed061be737e5765
Bhoomi jain <bhoomij0304@gmail.com>
2026-06-28 08:55:50 +0530

    Created helpers method

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,22 @@
 module ApplicationHelper
+
+  def status_badge(status)
+    if status == true
+      "🟢 Available"
+    else
+      "🔴 Not Available"
+    end
+  end
+
+  def experience_label(year)
+    if year >= 10
+      "Senior Doctor"
+    else
+      "Junior Doctor"
+    end
+  end
+
+  def current_year
+    Date.current.year 
+  end
 end
